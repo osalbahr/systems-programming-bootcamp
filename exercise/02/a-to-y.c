6c0f49e83c1a1c5e3c69d0a11cd8d81ebd6553dc
Osama Albahrani <osalbahr@gmail.com>
2026-05-04 16:06:35 +0300

    Add a-to-y

---
+++ exercise/02/a-to-y.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    char from = 'A';
+    printf("%c\n", from ^ 0b11000);
+}
---
+++ exercise/02/a-to-y.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+print(bin(ord("A")))
+print(bin(ord("Y")))
