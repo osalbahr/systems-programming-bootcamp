a3aa3ba0e39c1278a4ce73416d89b657cc11c5f5
Osama Albahrani <osalbahr@gmail.com>
2026-05-05 14:57:14 +0300

    Add 04-data-types.c

---
+++ repo-solutions/c-bootcamp/04-data-types.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 99;
+    double height = 199.50;
+    float weight = 200.80f;
+    char initial = 'O';
+
+    printf("age = %d\n", age);
+    printf("height = %lf\n", height);
+    printf("weight = %f\n", weight);
+    printf("initial = %c\n", initial);
+
+    return 0;
+}
