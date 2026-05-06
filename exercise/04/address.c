2c130b21c0949aed5d2c3cee322c99ca62ee790d
Osama Albahrani <osalbahr@gmail.com>
2026-05-06 15:30:00 +0300

    Add address.c

---
+++ exercise/04/address.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int value = 10;
+    int *ptr = &value;
+
+    // value -> &value
+    // ptr -> *ptr
+
+    printf("value: %d, address: %p\n", value, ptr);
+    printf("value: %d, address: %p\n", *ptr, &value);
+}
