e12464ac55e8726eab195add19697c7207878c1c
Osama Albahrani <osalbahr@gmail.com>
2026-05-10 17:01:08 +0300

    Add sum-dump.c

---
+++ exercise/05/sum-dump.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int values = 0x3b9dff;
+
+int sum(int a, int b)
+{
+    return a + b + values;
+}
+
+int main()
+{
+    unsigned char *sum_code = (unsigned char *)sum;
+    unsigned int code_size = 0;
+
+    // raw function
+    FILE *fp = fopen("sum.code", "rb");
+
+    for (;;) {
+        printf("%p -> %02x\n", sum_code, *sum_code);
+        code_size++;
+        
+        fwrite(sum_code, 1, 1, fp);
+
+        if(*sum_code == 0xc3) break;
+        sum_code++;
+    }
+
+    fclose(fp);
+
+    printf("code size: %u\n", code_size);
+}
