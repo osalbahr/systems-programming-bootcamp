54264f5669e859c5f3a6acb614b72862a3e282df
Osama Albahrani <osalbahr@gmail.com>
2026-05-12 15:59:14 +0300

    Add splitter.c (part 1)

---
+++ group-project/01/splitter.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("Usage: %s <filename> <count>\n", argv[0]);
+        exit(1);
+    }
+
+    char *filename = argv[1];
+
+    FILE *fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        printf("Cannot open '%s'\n", filename);
+        exit(1);
+    }
+
+    int split_count = atoi(argv[2]);
+    if (split_count <= 1) {
+        printf("Invalid count %d\n", split_count);
+        exit(1);
+    }
+
+    struct stat fileInfo;
+    fstat(fileno(fp), &fileInfo);
+    int block_size = (fileInfo.st_size + split_count - 1) / split_count;
+
+    for(int i = 0; i < split_count; i++){
+        char *buffer = (char *)malloc(block_size);
+        size_t bytes_read = fread(buffer, 1, block_size, fp);
+        printf("bytes_read = %zu\n", bytes_read);
+
+        char output_filename[1024];
+        sprintf(output_filename, "%s.%d", filename, i);
+
+        FILE *output = fopen(output_filename, "wb");
+        fwrite(buffer, 1, bytes_read, output);
+        fclose(output);
+    }
+}
